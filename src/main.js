ba56ba812ba86b6713c8490e96fe2e9a08693da4
minjeong-dev <vitaminjeong1026@gmail.com>
2022-04-11 23:27:08 +0900

    first coomit

---
+++ src/main.js
@@ -0,0 +1,9 @@
+const tshirtButton = document.querySelector('#menu .tshirt');
+const pantsButton = document.querySelector('#menu .pants');
+const skirtButton = document.querySelector('#menu .skirt');
+const blueButton = document.querySelector('#menu .blue');
+const yellowButton = document.querySelector('#menu .yellow');
+const pinkButton = document.querySelector('#menu .pink');
+const pants = document.querySelector('#list .');
+
+function clickTshirt() {}
